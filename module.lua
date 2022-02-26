dddd4cdca482a96d4181b93fbd20f605f2ee8fdb
Tom <93584455+FlysenDeveloppement@users.noreply.github.com>
2022-02-26 13:30:24 +0100

    Update module.lua

--- module.lua
+++ module.lua
@@ -1,7 +1,7 @@
 --[[
 	API:
 	  Documentation: https://devforum.roblox.com/t/timereasy-a-module-to-make-timer-more-easy/1681642
-	  Version: 1.0.0
+	  Version: 1.0.1
 	  
 	 
 	License: 
@@ -47,39 +47,10 @@ function Timer.new(second:number)
 	self.SpeedTime = 1
 	self.TimeChanged = Signal.new()
 	
-	return self
-end
-
-function Timer:Play()
-	if not self.IsPlaying then
-		self.IsPlaying = true
-		self.Time = self.BaseTime
-		spawn(function()
-			while self.IsPlaying and self.Time > 0 do
-				self.Time = self.Time - self.SecondMultiplier
-				self.TimeChanged:Fire(self.Time)
-				wait(self.SpeedTime)
-			end
-		end)
-	else
-		warn(debug.traceback("Timer is already playing."))
-	end
-end
-
-function Timer:Pause()
-	if self.IsPlaying then
-		self.IsPlaying = false
-	else
-		warn(debug.traceback("Timer is already paused"))
-	end
-end
-
-function Timer:Resume()
-	if self.Time == 0 then
-		warn(debug.traceback("Can't resume the Timer because the timer has ended."))
-	else
-		if self.IsPlaying == false then
+	function self:Play()
+		if not self.IsPlaying then
 			self.IsPlaying = true
+			self.Time = self.BaseTime
 			spawn(function()
 				while self.IsPlaying and self.Time > 0 do
 					self.Time = self.Time - self.SecondMultiplier
@@ -91,39 +62,68 @@ function Timer:Resume()
 			warn(debug.traceback("Timer is already playing."))
 		end
 	end
-end
 
-function Timer:AddTime(secondsToAdd:number)
-	if not secondsToAdd then error(debug.traceback("Missing 1 arguments to play the function.")) end
-	if not typeof(secondsToAdd) == "number" then error(debug.traceback("The secondsToAdd arguments must be a number")) end
+	function self:Pause()
+		if self.IsPlaying then
+			self.IsPlaying = false
+		else
+			warn(debug.traceback("Timer is already paused"))
+		end
+	end
+
+	function self:Resume()
+		if self.Time == 0 then
+			warn(debug.traceback("Can't resume the Timer because the timer has ended."))
+		else
+			if self.IsPlaying == false then
+				self.IsPlaying = true
+				spawn(function()
+					while self.IsPlaying and self.Time > 0 do
+						self.Time = self.Time - self.SecondMultiplier
+						self.TimeChanged:Fire(self.Time)
+						wait(self.SpeedTime)
+					end
+				end)
+			else
+				warn(debug.traceback("Timer is already playing."))
+			end
+		end
+	end
+
+	function self:AddTime(secondsToAdd:number)
+		if not secondsToAdd then error(debug.traceback("Missing 1 arguments to play the function.")) end
+		if not typeof(secondsToAdd) == "number" then error(debug.traceback("The secondsToAdd arguments must be a number")) end
+
+		self.Time = self.Time + secondsToAdd
+	end
+
+	function self:SkipTime(secondsToSkip:number)
+		if not secondsToSkip then error(debug.traceback("Missing 1 arguments to play the function.")) end
+		if not typeof(secondsToSkip) == "number" then error(debug.traceback("The secondsToSkip arguments must be a number")) end
+
+		self.Timer = self.Timer - secondsToSkip
+	end
+
+	function self:SetSecondMultiplier(newMultiplier:number)
+		if not newMultiplier then newMultiplier = 1 end
+		if not typeof(newMultiplier) == "number" then error(debug.traceback("The newMultiplier arguments must be a number")) end
+
+		self.SecondMultiplier = newMultiplier
+	end
+
+	function self:SetSpeedTime(newMultiplier:number)
+		if not newMultiplier then newMultiplier = 1 end
+		if not typeof(newMultiplier) == "number" then error(debug.traceback("The newMultiplier arguments must be a number")) end
+
+		self.SpeedTime = newMultiplier
+	end
+
+	function self:Stop()
+		self.IsPlaying = false
+		self.Timer = self.BaseTime
+	end
 	
-	self.Time = self.Time + secondsToAdd
-end
-
-function Timer:SkipTime(secondsToSkip:number)
-	if not secondsToSkip then error(debug.traceback("Missing 1 arguments to play the function.")) end
-	if not typeof(secondsToSkip) == "number" then error(debug.traceback("The secondsToSkip arguments must be a number")) end
-
-	self.Timer = self.Timer - secondsToSkip
-end
-
-function Timer:SetSecondMultiplier(newMultiplier:number)
-	if not newMultiplier then newMultiplier = 1 end
-	if not typeof(newMultiplier) == "number" then error(debug.traceback("The newMultiplier arguments must be a number")) end
-	
-	self.SecondMultiplier = newMultiplier
-end
-
-function Timer:SetSpeedTime(newMultiplier:number)
-	if not newMultiplier then newMultiplier = 1 end
-	if not typeof(newMultiplier) == "number" then error(debug.traceback("The newMultiplier arguments must be a number")) end
-	
-	self.SpeedTime = newMultiplier
-end
-
-function Timer:Stop()
-	self.IsPlaying = false
-	self.Timer = self.BaseTime
+	return self
 end
 
 return Timer
